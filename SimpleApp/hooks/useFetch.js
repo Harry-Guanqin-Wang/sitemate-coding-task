1de7237737ea2722cf7eabdcbee6328e0a1de374
Harry Wang <69313598+Harry-Guanqin-Wang@users.noreply.github.com>
2025-01-31 21:38:57 +1100

    feat(hooks): add custom hooks for handling api call, pass back data; loading; error

---
+++ SimpleApp/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (apiFunc) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await apiFunc();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [apiFunc]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
